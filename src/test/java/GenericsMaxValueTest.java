c54cb2194e362f5295e8f925f3d95e3c0485f8d4
paraglande03 <paraglande03@gmail.com>
2021-03-02 00:13:54 +0530

    Added: Test cases and methods for checking max float no.

--- src/test/java/GenericsMaxValueTest.java
+++ src/test/java/GenericsMaxValueTest.java
@@ -23,4 +23,21 @@ public class GenericsMaxValueTest {
         Integer maximum2 = findMax.testMaximumInteger(109, 200, 999);
         Assert.assertEquals((Integer) 999, maximum2);
     }
+    @Test
+    public void givenFirstFloatAsMaxNumber_ShouldReturnFirstFloatAsMaximum() {
+        Double max1 = findMax.testMaximumDouble(50.00, 5.00, 10.00);
+        Assert.assertEquals((Double) 50.00, max1);
+    }
+
+    @Test
+    public void givenSecondFloatAsMaxNumber_ShouldReturnSecondFloatAsMaximum() {
+        Double max1 = findMax.testMaximumDouble(10.00, 30.00, 20.00);
+        Assert.assertEquals((Double) 30.00, max1);
+    }
+    @Test
+    public void givenThirdFloatAsMaxNumber_ShouldReturnThirdFloatAsMaximum() {
+        Double max1 = findMax.testMaximumDouble(7.00, 23.00, 40.00);
+        Assert.assertEquals((Double) 40.00, max1);
+    }
+
 }
